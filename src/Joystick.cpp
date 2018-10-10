e9ca66be1b2e37fb47de8104c3bbfc609741960d
Vasiliy Polyakov <vp@psu.ru>
2018-10-10 07:26:31 +0500

    Initial commit

---
+++ src/Joystick.cpp
@@ -0,0 +1,36 @@
+﻿/**
+ * \file
+ * \brief   Joystick class implementation.
+ * \author  Vasiliy Polyakov <vp@psu.ru>
+ * \author  RobotPSU https://robot.psu.ru/
+ * \date    2018
+ */
+
+void Joystick::attach(uint8_t pinX, uint8_t pinY)
+{
+  _pinX = pinX;
+  _pinY = pinY;
+}
+
+void Joystick::attach(uint8_t pinX, uint8_t pinY, uint8_t pinSw)
+{
+  attach(pinX, pinY);
+  pinMode(_pinSw = pinSw, INPUT_PULLUP);
+}
+
+int Joystick::x()
+{
+  return analogRead(_pinX);
+}
+
+int Joystick::y()
+{
+  return analogRead(_pinY);
+}
+
+int Joystick::s()
+{
+  return _pinSw ? !digitalRead(_pinSw) : LOW;
+}
+
+/* vim: ft=arduino et sw=2 ts=2: */
---
+++ src/Joystick.h
@@ -0,0 +1,35 @@
+﻿/**
+ * \file
+ * \brief   Joystick class header.
+ * \author  Vasiliy Polyakov <vp@psu.ru>
+ * \author  RobotPSU https://robot.psu.ru/
+ * \date    2018
+ */
+
+#ifndef Joystick_h
+#define Joystick_h
+
+#include <Arduino.h>
+
+class Joystick
+{
+public:
+  Joystick() : _pinX(0), _pinY(0), _pinSw(0) {}
+
+  void attach(uint8_t pinX, uint8_t pinY);
+  void attach(uint8_t pinX, uint8_t pinY, uint8_t pinSw);
+
+  int x();
+  int y();
+  int s();
+
+private:
+  // Switch (button) pin
+  uint8_t _pinSw;
+  // Coordinate pins
+  uint8_t _pinX, _pinY;
+};
+
+#endif
+
+/* vim: set ft=arduino et sw=2 ts=2: */
